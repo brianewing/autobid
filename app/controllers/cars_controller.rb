7ad5e28d22251b302bcd9708f245abe9347b789e
Brian Ewing <me@brianewing.co.uk>
2012-04-21 19:05:13 +0100

    car creation and listing

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,13 +1,45 @@
 class CarsController < ApplicationController
+  include SafeParams
+
   before_filter :ensure_dealer
+  before_filter :assign_car, :only => [:new, :update, :edit]
+
+  safe :car, :name, :year, :model, :mileage, :color
+
+  def new; end
 
   def index
     @cars = Car.find_all_by_dealer_id(current_dealer.id)
   end
 
+  def create
+    car = safe_params
+    car[:model] = Model.find_by_id(car[:model])
+
+    @car = Car.by_dealer(current_dealer).create(car)
+
+    if @car.persisted?
+      flash[:info] = "Car has been created. Start an auction to begin selling"
+      redirect_to :cars
+    else
+      flash.now[:errors] = @car.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit; end
+
   private
 
   def ensure_dealer
     redirect_to :root if not current_dealer.present?
+  end
+
+  def assign_car
+    if params[:id]
+      @car = Car.by_dealer(current_dealer).find_by_id(params[:id])
+    end
+
+    @car ||= Car.new
   end
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,7 @@
 class Model < ActiveRecord::Base
   belongs_to :manufacturer
+
+  def full_name
+    "#{manufacturer.name} #{name}"
+  end
 end
