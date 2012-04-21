b650a39ff153d9d0c0579a32e37ee4a1f1a32e8f
Brian Ewing <me@brianewing.co.uk>
2012-04-21 17:00:48 +0100

    dealer validations

--- app/models/dealer.rb
+++ app/models/dealer.rb
@@ -1,6 +1,9 @@
 class Dealer < ActiveRecord::Base
   has_secure_password
 
+  validates_presence_of :name, :email, :password, :url
+  validates_uniqueness_of :email
+
   def self.login(email, password)
     find_by_email(email).try(:authenticate, password)
   end
