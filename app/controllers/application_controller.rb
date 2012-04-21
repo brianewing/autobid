a1c479a5f333c4e8db24970c6684f3ee13efd1a9
Brian Ewing <me@brianewing.co.uk>
2012-04-21 16:33:00 +0100

    solidified dealer creation, can't register if logged in, etc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def current_dealer
+    return nil unless session[:dealer_id]
+    @dealer ||= Dealer.find_by_id(session[:dealer_id])
+  end
+
+  def login!(dealer)
+    session[:dealer_id] = dealer.id
+  end
 end
--- app/controllers/dealers_controller.rb
+++ app/controllers/dealers_controller.rb
@@ -1,18 +1,35 @@
 class DealersController < ApplicationController
   include SafeParams
   before_filter :assign_dealer
+  before_filter :ensure_dealer, :only => [:update]
+  before_filter :ensure_no_dealer, :only => [:create, :new]
 
-  safe :dealer, :name, :url
+  safe :dealer, :name, :url, :email, :password
 
   def new; end
 
   def create
     @dealer = Dealer.create(safe_params)
+    
+    if @dealer.invalid?
+      flash.now[:errors] = @dealer.errors.full_messages
+      render :new
+    else
+      login! @dealer
+    end
   end
 
   private
 
   def assign_dealer
     @dealer ||= Dealer.new
+  end
+
+  def ensure_dealer
+    redirect_to root_url unless current_dealer.present?
+  end
+
+  def ensure_no_dealer
+    redirect_to root_url if current_dealer.present?
   end
 end
