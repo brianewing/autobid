4629fb3be7f93d0d370ff2f12fda34fc6da5f345
Brian Ewing <me@brianewing.co.uk>
2012-04-21 16:10:00 +0100

    can now create dealers (use safe_params)

--- app/controllers/dealers_controller.rb
+++ app/controllers/dealers_controller.rb
@@ -1,7 +1,14 @@
 class DealersController < ApplicationController
+  include SafeParams
   before_filter :assign_dealer
 
+  safe :dealer, :name, :url
+
   def new; end
+
+  def create
+    @dealer = Dealer.create(safe_params)
+  end
 
   private
 
